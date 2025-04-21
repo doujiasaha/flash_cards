1673a0bb9ab735a37dea0de2c2449c5165143733
Asakura <Asakura-MBA>
2025-04-21 11:38:05 +0200

    Added functions to get a new card and flip it

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
+# ---------------------------- IMPORTS ------------------------------- #
 from tkinter import *
 import random
+import pandas
 
+# ---------------------------- GLOBALS ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
+timer_var = None
+spanish = ""
+english = ""
 
+# ---------------------------- DATA READING ------------------------------- #
 
+data = pandas.read_csv("data/spanish_words.csv").to_dict()
 
+#Dict comprehension mapping spanish to english word
+wordlist = {data["spanish"][i]:data["english"][i] for i in data["spanish"]}
 
+# ---------------------------- FILLING FLASH CARD ------------------------------- #
+
+def flash_card():
+    global spanish, english
+    #Grab random spanish word and corresponding enlish word    
+    spanish, english = random.choice(list(wordlist.items()))
+    print(f"Selected: {spanish} -> {english}")  # Debug print
+    
+    canvas.itemconfig(card_image, image=front)
+    canvas.itemconfig(front_language, fill="black",text=f"Spanish")
+    canvas.itemconfig(front_word,fill="black",text=f"{spanish}")
+    countdown(3)
+
+# ---------------------------- FLIP FLASH CARD ------------------------------- #
+
+def flip():
+    print(f"Selected: {spanish} -> {english}")  # Debug print
+    canvas.itemconfig(card_image, image=back)
+    canvas.itemconfig(front_language, fill="white",text=f"English")
+    canvas.itemconfig(front_word,fill="white",text=f"{english}")
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+def countdown(count):    
+    if count > 0:
+            global timer_var
+            timer_var = window.after(1000, countdown,count -1)
+    elif count == 0:
+        flip()
+        
 # ---------------------------- UI SETUP ------------------------------- #
 #Basic window
 window = Tk()
@@ -17,18 +57,18 @@ window.config(padx=50,pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 front = PhotoImage(file="images/card_front.png")
 back = PhotoImage(file="images/card_back.png")
-canvas.create_image(400,263,image=front)
-front_word = canvas.create_text(400,263, text="Spanish",fill="Black",font=("arial",35,"bold"))
-front_text = canvas.create_text(400,163, text="English",fill="Black",font=("arial",35,"italic"))
+card_image = canvas.create_image(400,263,image=front)
+front_language = canvas.create_text(400,163, text="Spanish",fill="Black",font=("arial",35,"italic"))
+front_word = canvas.create_text(400,263, text="English",fill="Black",font=("arial",35,"bold"))
 canvas.grid(column=0, row=0,columnspan=2)
 
 #Wrong and correct buttons
 cross_image = PhotoImage(file="images/wrong.png")
-cross_button = Button(image=cross_image,highlightthickness=0,border=0)
+cross_button = Button(image=cross_image,highlightthickness=0,border=0, command=flash_card)
 cross_button.grid(column=0,row=1)
 
 check_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_image,highlightthickness=0,border=0)
+check_button = Button(image=check_image,highlightthickness=0,border=0, command=flash_card)
 check_button.grid(column=1,row=1)
 
 
